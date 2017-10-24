2f98f386a0fdaffd5e3e5854be07e20e3c19e2d9
DESKTOP-K89EU66\MORAGUESFN <francisco.moragues@davinci.edu.ar>
2017-10-23 22:13:53 -0300

    Se reemplaza el Prefab que levanta los idiomas por una Instancia.

--- Assets/Editor/LanguageManager.cs
+++ Assets/Editor/LanguageManager.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-
 [ExecuteInEditMode]
 public class LanguageManager : EditorWindow
 {
     private const string notFoundTextString = "Key not found in dictionary{0}";
 
     public static event System.Action onReload;
-
     public SystemLanguage defaultLanguage = SystemLanguage.English;
     public SystemLanguage currentLanguage;
     public LanguageResource[] resources;
@@ -37,13 +35,14 @@ public class LanguageManager : EditorWindow
         {
             if (s_instance == null)
             {
-#if UNITY_EDITOR
+
                 s_instance = FindObjectOfType<LanguageManager>();
                 if (s_instance == null)
                 {
-                    s_instance = Instantiate(UnityEditor.AssetDatabase.LoadAssetAtPath<LanguageManager>(UnityEditor.AssetDatabase.GUIDToAssetPath(UnityEditor.AssetDatabase.FindAssets("LanguageManager t:prefab")[0])));
+                    //LanguageResource[] s_instance = (LanguageResource[])Resources.FindObjectsOfTypeAll(typeof(LanguageResource));
+                    //s_instance = Instantiate(UnityEditor.AssetDatabase.LoadAssetAtPath<LanguageManager>(UnityEditor.AssetDatabase.GUIDToAssetPath(UnityEditor.AssetDatabase.FindAssets("LanguageManager t:prefab")[0])));
                 }
-#endif
+
             }
             else
             {
@@ -54,7 +53,7 @@ public class LanguageManager : EditorWindow
         }
     }
 
-    [MenuItem("PlugIn/Translation")]
+    [MenuItem("Plugin/Translation")]
     public static void ShowWindows()
     {
         ((LanguageManager)GetWindow(typeof(LanguageManager))).Show();
@@ -62,12 +61,13 @@ public class LanguageManager : EditorWindow
 
     void OnGUI()
     {
-        if (s_instance != null)
+
+        EditorGUILayout.LabelField("Object", EditorStyles.boldLabel);
+        /*if (s_instance != null)
         {
-            //Destroy(this.gameObject);
-            //Destroy(this);
-            //return;
-        }
+            Destroy(this);
+            return;
+        }*/
 
         if (Application.systemLanguage != SystemLanguage.Unknown)
             currentLanguage = Application.systemLanguage;
@@ -78,16 +78,14 @@ public class LanguageManager : EditorWindow
         LoadLanguage(currentLanguage);
 
         if(Application.isPlaying)
-            //DontDestroyOnLoad(gameObject);
             DontDestroyOnLoad(this);
 
     }
 
     public static void LoadLanguage(SystemLanguage lang)
     {
-        int i = 0;
-        LanguageResource[] rsc = s_instance.resources;
-        for (i = 0; i < rsc.Length; i++)
+        LanguageResource[] rsc = (LanguageResource[])Resources.FindObjectsOfTypeAll(typeof(LanguageResource));
+        for (int i = 0; i < rsc.Length; i++)
         {
             if (rsc[i].language == lang)
             {
